ecf9a712a193ac6c1c2a760e9421f68950951e2d
codecrook <hello@codecrook.io>
2020-01-26 16:29:26 +0530

    Created the base UI

--- main.js
+++ main.js
@@ -1,4 +1,26 @@
 {
+    class HelperFunctions {
+        getMessageElement(data) {
+            const newElement = document.createElement('div');
+            newElement.innerHTML = `${data.name === CURRENT_USER ? '' : data.name+'<br/>'}
+                                    ${data.message}`;
+            newElement.className = `message ${data.name === CURRENT_USER ? 'mine' : ''}`;
+        
+            return newElement;
+        }
+    }
     const inputBox = document.querySelector('#inputbox');
     const messages = document.querySelector('#messages');
+    const msgHelpFuncs = new HelperFunctions();
+    const CURRENT_USER = '@codecrook';
+
+    messages.appendChild(msgHelpFuncs.getMessageElement({ name: '@codecrook', message: 'Hello' }));
+    messages.appendChild(msgHelpFuncs.getMessageElement({ name: '@chethan', message: 'Hi' }));
+
+    inputBox.addEventListener('keyup', e => { 
+        if (e && e.key === 'Enter' && inputBox.value.trim !== 0) {
+            messages.appendChild(msgHelpFuncs.getMessageElement({ name: CURRENT_USER, message: e.target.value }));
+            inputBox.value = '';
+        }
+    });
 }
